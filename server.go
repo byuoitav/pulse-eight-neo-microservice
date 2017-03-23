314c5ee1b59c665b8f8143464897fcb7c3e11cc0
Jesse Millar <hellojessemillar@gmail.com>
2017-03-23 13:10:34 -0600

    Making progress

--- server.go
+++ server.go
@@ -6,19 +6,19 @@ import (
 
 	"github.com/byuoitav/authmiddleware"
 	"github.com/byuoitav/hateoas"
-	"github.com/byuoitav/sony-control-microservice/handlers"
+	"github.com/byuoitav/pulse-eight-neo-microservice/handlers"
 	"github.com/jessemillar/health"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	err := hateoas.Load("https://raw.githubusercontent.com/byuoitav/sony-control-microservice/master/swagger.json")
+	err := hateoas.Load("https://raw.githubusercontent.com/byuoitav/pulse-eight-neo-microservice/master/swagger.json")
 	if err != nil {
 		log.Fatalln("Could not load swagger.json file. Error: " + err.Error())
 	}
 
-	port := ":8007"
+	port := ":8011"
 	router := echo.New()
 	router.Pre(middleware.RemoveTrailingSlash())
 	router.Use(middleware.CORS())
