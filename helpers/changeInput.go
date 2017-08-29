d557d267f669704f1ce83c0ea7c091126bf31b64
Joseph Blodgett <joseph.blodgett@gmail.com>
2017-08-28 19:53:11 -0600

    Moving resp.Body.Close()
    We need to move it until we're sure that there IS a body.

--- helpers/changeInput.go
+++ helpers/changeInput.go
@@ -10,11 +10,11 @@ import (
 
 func SwitchInput(address string, input string, output string) error {
 	resp, err := http.Get(fmt.Sprintf("http://%s/Port/Set/%s/%s", address, input, output))
-	defer resp.Body.Close()
-
 	if err != nil {
 		return err
 	}
+	defer resp.Body.Close()
+	
 	if resp.StatusCode != 200 {
 		responseBody, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
